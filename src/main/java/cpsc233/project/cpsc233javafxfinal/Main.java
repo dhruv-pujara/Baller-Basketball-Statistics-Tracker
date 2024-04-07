c849c09932b32ca14ed8ddde577b49a2cfc65ad8
gavin <gavin.walters@ucalgary.ca>
2024-04-07 15:05:09 -0600

    Added GUI drawing, Initialized all respositories, added files

--- src/main/java/cpsc233/project/cpsc233javafxfinal/Main.java
+++ src/main/java/cpsc233/project/cpsc233javafxfinal/Main.java
@@ -3,21 +3,36 @@ package cpsc233.project.cpsc233javafxfinal;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(OldMain.class.getResource("Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Baller: The Basketball Tracking Program v1.3");
+        stage.setTitle("Baller");
+        stage.setOnCloseRequest(e -> ExitProgram(e, stage));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void ExitProgram(javafx.event.Event event, Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("Baller is about to be closed");
+        alert.setContentText("Would you like to save your file before exiting? If so click cancel and go save!");
+        Optional<ButtonType> closeornot = alert.showAndWait();
+        if (closeornot.isPresent() && (closeornot.get() == ButtonType.CANCEL || closeornot.get() == ButtonType.CLOSE)) {
+            event.consume();
+        }
     }
 }
