7bacaa2808f032c1bbc14d0b3363f7d0389bd7ca
gavin <gavin.walters@ucalgary.ca>
2024-04-06 19:40:03 -0600

    Added GUI drawing, Initialized all respositories

--- src/main/java/cpsc233/project/cpsc233javafxfinal/PleaseProvideControllerClassName.java
+++ src/main/java/cpsc233/project/cpsc233javafxfinal/PleaseProvideControllerClassName.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -13,7 +12,8 @@ import javafx.scene.text.Font;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class PleaseProvideControllerClassName implements Initializable {
+
         @FXML
         private ComboBox<?> playerSelect;
 
@@ -60,8 +60,8 @@ public class MainController implements Initializable {
 
         }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
+        }
 }
