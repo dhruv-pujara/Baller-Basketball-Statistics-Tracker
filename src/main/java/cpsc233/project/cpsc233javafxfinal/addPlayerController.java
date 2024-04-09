1067b028d1b6250c11f35bd93ca18af9f9b6110d
gavin <gavin.walters@ucalgary.ca>
2024-04-08 20:52:11 -0600

    I think addgame should work now?

--- src/main/java/cpsc233/project/cpsc233javafxfinal/addPlayerController.java
+++ src/main/java/cpsc233/project/cpsc233javafxfinal/addPlayerController.java
@@ -1,5 +1,18 @@
 package cpsc233.project.cpsc233javafxfinal;
 
-public class addPlayerController{
+public class addPlayerController {
+
+    private MainInterfaceController mainInterfaceController;
+    private Team team;
+    private Player player;
+
+    public void initData(Team team, Player player) {
+        this.team = team;
+        this.player = player;
+    }
+
+    public void setMainInterfaceController(MainInterfaceController mainInterfaceController) {
+        this.mainInterfaceController = mainInterfaceController;
+    }
 
 }
