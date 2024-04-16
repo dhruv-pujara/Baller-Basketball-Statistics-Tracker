6f5b727e39e5bce1df2f324c08f3418f7596ef39
dpuja <dhruv.pujara1@ucalgary.ca>
2024-04-15 20:00:12 -0600

    updated jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module cpsc233.project.cpsc233javafxfinal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
+    requires java.xml;
 
 
     opens cpsc233.project.cpsc233javafxfinal to javafx.fxml;
